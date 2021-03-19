97311328c9ad3cd14b975dd3d9c36d65e02a0e00
suhanabhargava <76730859+suhanabhargava@users.noreply.github.com>
2021-03-19 19:50:42 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Box Class
+# angryBirdsStage2StudentActivity
+Student Activity Link for Angry Birds Stage 2
--- sketch.js
+++ sketch.js
@@ -3,51 +3,59 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1 , box2 , box3 , box4 , box5;
-var ground1;
-var pig1 , pig2;
-var log1 , log2 , log3 , log4;
-var bird1;
+var box1, pig1;
+var backgroundImage;
 
+function preload(){
+    backgroundImage=loadImage("sprites/bg.png");
+}
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-    pig1 = new pig (810,350);
-    pig2 = new pig (810,220);
-    log1 = new log (810,260,300,PI/2);
-    log2 = new log (810,180,300,PI/2);
-    log4 = new log (760,120,150,PI/7);
-    log5 = new log (870,120,150,-PI/7);
-    bird1 = new bird (60,60);
 
+    
+    ground = new Ground(600,height,1200,20)
 
-
-    box2 = new Box(920,320,70,70);
-    box3 = new Box(700,240,70,70);
     box1 = new Box(700,320,70,70);
-    box4 = new Box(920,240,70,70);
-    box5 = new Box(800,150,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
 
-    ground1 = new ground(600,390,1200,10);
-  
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(100,100);
+
 }
 
 function draw(){
-    background(0);
+    background(backgroundImage);
     Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
     box1.display();
     box2.display();
-    ground1.display();
+    ground.display();
     pig1.display();
     log1.display();
-    log2.display();
+
     box3.display();
     box4.display();
-    pig2.display();
+    pig3.display();
+    log3.display();
+
     box5.display();
     log4.display();
     log5.display();
-    bird1.display();
 
+    bird.display();
 }
